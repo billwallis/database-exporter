221f9cabfb4a0c2392768383c627090f2a31a7a0
Bill <bill@thewallisfamily.co.uk>
2024-11-25 08:51:00 +0000

    test: refactor tests

--- tests/end-to-end/test__end_to_end.py
+++ tests/end-to-end/test__end_to_end.py
@@ -14,47 +14,42 @@ from database_exporter.exporter import DatabaseConnection
 FIXTURES = pathlib.Path(__file__).parent / "fixtures"
 
 
-@pytest.fixture
-def simple_query__sql() -> str:
-    return (FIXTURES / "simple_query.sql").read_text().strip()
+def _read_fixture(filename: str) -> str:
+    return (FIXTURES / filename).read_text().strip()
+
+
+class QueryFixture:
+    sql: str
+    csv: str
+    jsonl: str
+
+    def __init__(self, name: str):
+        self.sql = _read_fixture(f"{name}.sql")
+        self.csv = _read_fixture(f"{name}.csv")
+        self.jsonl = _read_fixture(f"{name}.jsonl")
 
 
 @pytest.fixture
-def simple_query__csv() -> str:
-    return (FIXTURES / "simple_query.csv").read_text().strip()
+def simple_query() -> QueryFixture:
+    return QueryFixture("simple_query")
 
 
 @pytest.fixture
-def simple_query__jsonl() -> str:
-    return (FIXTURES / "simple_query.jsonl").read_text().strip()
-
-
-@pytest.fixture
-def complex_query__sql() -> str:
-    return (FIXTURES / "complex_query.sql").read_text().strip()
-
-
-@pytest.fixture
-def complex_query__csv() -> str:
-    return (FIXTURES / "complex_query.csv").read_text().strip()
-
-
-@pytest.fixture
-def complex_query__jsonl() -> str:
-    return (FIXTURES / "complex_query.jsonl").read_text().strip()
+def complex_query() -> QueryFixture:
+    return QueryFixture("complex_query")
 
 
 @pytest.mark.parametrize(
-    "query, result, exporter",
+    "query_fixture, result_type, exporter",
     [
         (
-            "simple_query__sql",
-            "simple_query__csv",
+            "simple_query",
+            "csv",
             database_exporter.query_to_csv,
         ),
         (
-            "simple_query__sql",
-            "simple_query__jsonl",
+            "simple_query",
+            "jsonl",
             database_exporter.query_to_jsonl,
         ),
     ],
@@ -63,17 +58,20 @@ def test__sqlite_result_sets_can_be_exported_to_a_file(
     sqlite_connection: DatabaseConnection,
     tmp_path: pathlib.Path,
     request: pytest.FixtureRequest,
-    query: str,
-    result: str,
+    query_fixture: str,
+    result_type: str,
     exporter: Callable,
 ) -> None:
     """
     The result set from a SQLite query can be exported to a file.
     """
+    query: QueryFixture = request.getfixturevalue(query_fixture)
+    result = getattr(query, result_type)
+
     actual_path = tmp_path / "actual.file"
     exporter(
         conn=sqlite_connection,
-        query=request.getfixturevalue(query),
+        query=query.sql,
         filepath=actual_path,
     )
 
@@ -83,7 +81,7 @@ def test__sqlite_result_sets_can_be_exported_to_a_file(
     # The sqlite3 driver returns ints instead of bools
     # fmt: off
     result = (
-        request.getfixturevalue(result)
+        result
             .replace("true", "1")
             .replace("false", "0")
     )
@@ -96,26 +94,26 @@ def test__sqlite_result_sets_can_be_exported_to_a_file(
 
 
 @pytest.mark.parametrize(
-    "query, result, exporter",
+    "query_fixture, result_type, exporter",
     [
         (
-            "simple_query__sql",
-            "simple_query__csv",
+            "simple_query",
+            "csv",
             database_exporter.query_to_csv,
         ),
         (
-            "complex_query__sql",
-            "complex_query__csv",
+            "complex_query",
+            "csv",
             database_exporter.query_to_csv,
         ),
         (
-            "simple_query__sql",
-            "simple_query__jsonl",
+            "simple_query",
+            "jsonl",
             database_exporter.query_to_jsonl,
         ),
         (
-            "complex_query__sql",
-            "complex_query__jsonl",
+            "complex_query",
+            "jsonl",
             database_exporter.query_to_jsonl,
         ),
     ],
@@ -124,17 +122,18 @@ def test__duckdb_result_sets_can_be_exported_to_a_file(
     duckdb_connection: DatabaseConnection,
     tmp_path: pathlib.Path,
     request: pytest.FixtureRequest,
-    query: str,
-    result: str,
+    query_fixture: str,
+    result_type: str,
     exporter: Callable,
 ) -> None:
     """
     The result set from a DuckDB query can be exported to a file.
     """
+    query: QueryFixture = request.getfixturevalue(query_fixture)
     actual_path = tmp_path / "actual.file"
     exporter(
         conn=duckdb_connection,
-        query=request.getfixturevalue(query),
+        query=query.sql,
         filepath=actual_path,
     )
 
@@ -144,4 +143,4 @@ def test__duckdb_result_sets_can_be_exported_to_a_file(
     # In CI, the drivers print extra line breaks to the CSV for some reason :shrug:
     actual = re.sub(r"\n+", "\n", actual)
 
-    assert actual == request.getfixturevalue(result)
+    assert actual == getattr(query, result_type)
